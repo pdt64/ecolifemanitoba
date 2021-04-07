85adc12ce2ea38b1ee5c2907de0de01e7973056a
Piolo Turdanes <piolot64@gmail.com>
2021-04-07 01:17:48 -0500

    Added image upload to edit form.

--- edit.php
+++ edit.php
@@ -6,36 +6,108 @@
    	require 'connection.php';
     require 'header.php';
 
-	$title  = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    include '\xampp\htdocs\TermProject\uploads\php-image-resize-master\lib\ImageResize.php';
+    include '\xampp\htdocs\TermProject\uploads\php-image-resize-master\lib\ImageResizeException.php';
+
+    use \Gumlet\ImageResize;
+
+    function file_is_valid($temporary_path, $new_path) {
+        $allowed_mime_types      = ['image/jpeg', 'image/jpg', 'image/png'];
+        $allowed_file_extensions = ['JPG', 'jpg', 'JPEG', 'jpeg', 'png'];
+
+        $actual_file_extension   = pathinfo($new_path, PATHINFO_EXTENSION);
+        $actual_mime_type        = mime_content_type($temporary_path);
+
+        $file_extension_is_valid = in_array($actual_file_extension, $allowed_file_extensions);
+        $mime_type_is_valid      = in_array($actual_mime_type, $allowed_mime_types);
+
+        return $file_extension_is_valid && $mime_type_is_valid;
+    }
+
+    function file_upload_path($original_filename, $upload_subfolder_name = 'uploads') {
+       $current_folder = dirname(__FILE__);
+       $path_segments = [$current_folder, $upload_subfolder_name, basename($original_filename)];
+       return join(DIRECTORY_SEPARATOR, $path_segments);
+    }
+
+	  $title  = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
   	$content = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
   	$id      = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
+    $image_query     = "UPDATE blogposts SET title = :title, content = :content, dateUpdated = CURRENT_TIMESTAMP, image = :image WHERE postid = :id";
   	$update_query     = "UPDATE blogposts SET title = :title, content = :content, dateUpdated = CURRENT_TIMESTAMP WHERE postid = :id";
   	$delete_query = "DELETE FROM blogposts WHERE postid = :id"; 	
   	$select_query = "SELECT * FROM blogposts WHERE postid = :id";
 
-  	$select_statement = $db->prepare($select_query);
-  	$select_statement->bindValue(':id', $id, PDO::PARAM_INT);
- 	$select_statement->execute(); 
-  	$post = $select_statement->fetchAll();
+    $select_statement = $db->prepare($select_query);
+    $select_statement->bindValue(':id', $id, PDO::PARAM_INT);
+    $select_statement->execute(); 
+    $post = $select_statement->fetchAll();
+
+    if(filter_var($id, FILTER_VALIDATE_INT)){
+    if(isset($_POST['title']) && strlen($_POST['title']) >= 1 && 
+      isset($_POST['content']) && strlen($_POST['content']) >= 1 && isset($_FILES['image']) 
+      && isset($_POST['update'])){
+
+    $image_upload_detected = isset($_FILES['image']) && ($_FILES['image']['error'] === 0);
+
+    if ($image_upload_detected) {
+        $image_filename       = $_FILES['image']['name'];
+        $temporary_image_path = $_FILES['image']['tmp_name'];
+        $new_image_path       = file_upload_path($image_filename);
+
+          if(file_is_valid($temporary_image_path, $new_image_path)) {
+
+            move_uploaded_file($temporary_image_path, $new_image_path);
+
+            $path_parts = pathinfo($new_image_path);
+
+              $image = new \Gumlet\ImageResize($new_image_path);
+
+                  $file_components = explode('.', $image_filename);
+
+                  $image
+                      ->resizeToWidth(400)
+                      ->save($file_components[0] . "_medium." . $file_components[1])
+
+                      ->resizeToWidth(50)
+                      ->save($file_components[0] . "_thumbnail." . $file_components[1])
+                  ;
+            $image = $_FILES['image']['name'];
+      
+            $statement = $db->prepare($image_query);
+
+            $statement->bindValue(':image', $image);
+            $statement->bindValue(':title', $title);
+            $statement->bindValue(':content', $content);
+            $statement->bindValue(':id', $id, PDO::PARAM_INT);
+
+            $statement->execute();
+
+            header('Location: index.php');
+            exit();
+          } else {
+            $statement = $db->prepare($update_query);
+
+            $statement->bindValue(':title', $title);
+            $statement->bindValue(':content', $content);
+            $statement->bindValue(':id', $id, PDO::PARAM_INT);
+
+            $statement->execute();
+
+            header('Location: index.php');
+            exit();
+    }
+    
+  }
+}
+}
 
 
   	if(count($post) == 0){
   		header('Location: index.php');
   		exit();
   	} else {
-  	if(filter_var($id, FILTER_VALIDATE_INT)){
-		if(isset($_POST['title']) && strlen($_POST['title']) >= 1 && 
-		    isset($_POST['content']) && strlen($_POST['content']) >= 1 && isset($_POST['update'])) {
-			$update_statement = $db->prepare($update_query);
-			$update_statement->bindValue(':title', $title);
-  			$update_statement->bindValue(':content', $content);
-  			$update_statement->bindValue(':id', $id, PDO::PARAM_INT);
-			$update_statement->execute();
-
-		 	header('Location: index.php');
-		    exit();
-		}
 
 	    if(isset($_POST['delete'])){
 	    	$delete_statement = $db->prepare($delete_query);
@@ -44,11 +116,10 @@
 	  		header('Location: index.php');
 	  		exit();
 	  	}
-	} else {
-		header('Location: index.php');
-		exit();
-	}
+
   }
+
+
   if(!isset($_SESSION)){
     session_start();
   }
@@ -66,7 +137,7 @@
 <?php if(isset($_SESSION)): ?>
     <div id="wrapper">
 	<div id="all_blogs">
-	  <form method="post">
+	  <form method="post" enctype="multipart/form-data">
 	    <fieldset>
 	      <legend>Edit Blog Post</legend>
 	      <p>
@@ -77,6 +148,13 @@
 	        <label for="content">Content</label>
 	        <textarea name="content" id="content" cols="100" rows="20"><?= $post[0]['content'] ?></textarea>
 	      </p>
+        <?php if($post[0]['image'] != null): ?>
+          <p>Existing image:</p>
+          <p><?= $post[0]['image'] ?></p>
+        <?php else: ?>
+          <label for="image">Image Filename:</label>
+          <input type="file" name="image" id="image"/>
+        <?php endif ?>
 	      <p>
 	        <input type="hidden" name="id" value="<?= $post[0]['postId'] ?>" />
 	        <input type="submit" name="update" value="Update" />
