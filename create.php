4a11f837bdfe9e6709d5a872b30c3b2011fd446c
Piolo Turdanes <piolot64@gmail.com>
2021-04-07 00:16:01 -0500

    Added image uploads to the create form.

--- create.php
+++ create.php
@@ -4,25 +4,90 @@
   // Date: 
   // Purpose: Allows users to create posts and insert them into the database.
 
-
+  
 
   require 'connection.php';
   require 'header.php';
 
+  include '\xampp\htdocs\TermProject\uploads\php-image-resize-master\lib\ImageResize.php';
+  include '\xampp\htdocs\TermProject\uploads\php-image-resize-master\lib\ImageResizeException.php';
+
+  use \Gumlet\ImageResize;
+
+    function file_is_valid($temporary_path, $new_path) {
+        $allowed_mime_types      = ['image/jpeg', 'image/jpg', 'image/png'];
+        $allowed_file_extensions = ['JPG', 'jpg', 'JPEG', 'jpeg', 'png'];
+
+        $actual_file_extension   = pathinfo($new_path, PATHINFO_EXTENSION);
+        $actual_mime_type        = mime_content_type($temporary_path);
+
+        $file_extension_is_valid = in_array($actual_file_extension, $allowed_file_extensions);
+        $mime_type_is_valid      = in_array($actual_mime_type, $allowed_mime_types);
+
+        return $file_extension_is_valid && $mime_type_is_valid;
+    }
+
+    function file_upload_path($original_filename, $upload_subfolder_name = 'uploads') {
+       $current_folder = dirname(__FILE__);
+       $path_segments = [$current_folder, $upload_subfolder_name, basename($original_filename)];
+       return join(DIRECTORY_SEPARATOR, $path_segments);
+    }
+
   $title  = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
   $content = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-  $query     = "INSERT INTO blogposts (title, content) values (:title, :content)";
 
-  $statement = $db->prepare($query);
-  $statement->bindValue(':title', $title);
-  $statement->bindValue(':content', $content);
+  $image_query     = "INSERT INTO blogposts (title, content, image) values (:title, :content, :image)";
+  $query = "INSERT INTO blogposts (title, content) values (:title, :content)";
   
   if(isset($_POST['title']) && strlen($_POST['title']) >= 1 && 
-      isset($_POST['content']) && strlen($_POST['content']) >= 1){
-    $statement->execute();
-    $insert_id = $db->lastInsertId();
+      isset($_POST['content']) && strlen($_POST['content']) >= 1 && isset($_FILES['image'])){
 
+    $image_upload_detected = isset($_FILES['image']) && ($_FILES['image']['error'] === 0);
+
+    if ($image_upload_detected) {
+        $image_filename       = $_FILES['image']['name'];
+        $temporary_image_path = $_FILES['image']['tmp_name'];
+        $new_image_path       = file_upload_path($image_filename);
+
+          if(file_is_valid($temporary_image_path, $new_image_path)) {
+
+            move_uploaded_file($temporary_image_path, $new_image_path);
+
+            $path_parts = pathinfo($new_image_path);
+
+              $image = new \Gumlet\ImageResize($new_image_path);
+
+                  $file_components = explode('.', $image_filename);
+
+                  $image
+                      ->resizeToWidth(400)
+                      ->save($file_components[0] . "_medium." . $file_components[1])
+
+                      ->resizeToWidth(50)
+                      ->save($file_components[0] . "_thumbnail." . $file_components[1])
+                  ;
+            $image = $_FILES['image']['name'];
+      
+            $statement = $db->prepare($image_query);
+
+            $statement->bindValue(':image', $image);
+            $statement->bindValue(':title', $title);
+            $statement->bindValue(':content', $content);
+
+            $statement->execute();
+            $insert_id = $db->lastInsertId(); 
+          } else {
+            $statement = $db->prepare($query);
+
+            $statement->bindValue(':title', $title);
+            $statement->bindValue(':content', $content);
+
+            $statement->execute();
+            $insert_id = $db->lastInsertId(); 
+    }
+    
+  }
     header('Location: index.php');
     exit();
   }
@@ -44,7 +109,7 @@
 <?php if(isset($_SESSION['userId'])): ?>
   <div id="wrapper">
     <div id="all_blogs">
-      <form method="post">
+      <form method="post" enctype="multipart/form-data">
         <fieldset>
           <legend>New Blog Post</legend>
           <p>
@@ -55,9 +120,9 @@
             <label for="content">Content</label>
             <textarea name="content" id="content" cols="100" rows="20"></textarea>
           </p>
-          <p>
-            <input type="submit" name="command" value="Create"  />
-          </p>
+          <label for="image">Image Filename:</label>
+          <input type="file" name="image" id="image"/>
+            <input type="submit" name="submit" value="Create"  />
           <?php if(isset($_POST['title']) && strlen($_POST['title']) < 1 && 
           isset($_POST['content']) && strlen($_POST['content']) < 1): ?>
             <p>Title and content may not be less than 1 character.</p>
